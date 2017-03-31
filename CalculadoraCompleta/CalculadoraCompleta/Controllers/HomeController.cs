a82f5d34b4f3abef3f5c9424af120d2b571bca09
Tmatos <aluno19270@ipt.pt>
2017-03-31 16:39:41 +0100

    codigo associado ao processamento dos algarismos

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -11,14 +11,48 @@ namespace CalculadoraCompleta.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //inicializar os dados a serem enviados para a view
+            ViewBag.Visor = "0";
             return View();
         }
         // POST: Home
         [HttpPost]
-        public ActionResult Index(string bt)
+        public ActionResult Index(string bt, string visor)
         {
             //determinar o conteudo do botao
+            switch (bt)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    if (visor.Equals("0")) { visor = bt; }
+                    else visor += bt;
+                    break;
 
+                case "+/-" :
+                    //visor = Convert.ToDouble(visor) * -1 + "";
+                    if (visor.StartsWith("-")) visor = visor.Replace("-", "");
+                    else visor = "-" + visor;
+                    break;
+                case ",":
+                    if (!visor.Contains(",")) visor += ",";
+                    break;
+                case "C":
+                    visor = "0";
+                    break;
+
+            }
+
+            // enviar dados para a view
+
+            ViewBag.Visor = visor;
             return View();
         }
     }
